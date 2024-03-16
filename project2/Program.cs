1deeabc2c7a43cb0ce0bfde65f4c6174626800e4
Scott <scl5463@rit.edu>
2024-03-16 09:27:33 -0400

    have new number getting method and placeholder for isProbablyPrime extension

--- project2/Program.cs
+++ project2/Program.cs
@@ -1,5 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.Numerics;
+using System.Security.Cryptography;
 
 namespace project2
 {
@@ -13,21 +13,18 @@ namespace project2
 
         static void Main(string[] args)
         {
+            int count;
             try
             {
-                if (args.Length < 2 || args.Length > 3)
+                if (
+                    args.Length < 2
+                    || args.Length > 3
+                    || !validBits(args[0])
+                    || !validOption(args[1])
+                )
                 {
                     Console.WriteLine(error_message);
                     return;
-                }
-                if (!validBits(args[0]))
-                {
-                    Console.WriteLine(error_message);
-                    return;
-                }
-                if (!validOption(args[1]))
-                {
-                    Console.WriteLine(error_message);
                 }
                 if (args.Length == 3)
                 {
@@ -36,13 +33,28 @@ namespace project2
                         Console.WriteLine(error_message);
                         return;
                     }
+                    count = int.Parse(args[2]);
                 }
+                else
+                {
+                    count = 1;
+                }
+                string bits = args[0];
+                string option = args[1];
+
+                var bi = new BigInteger(getNumber(bits));
+                Console.WriteLine(bi);
+                // get primes
+
+                // get odds
             }
-            catch (Exception ex)
+            catch
             {
                 Console.WriteLine(error_message);
             }
         }
+
+        static void getOdds(int number, int count) { }
 
         static bool validBits(string bits)
         {
@@ -51,7 +63,7 @@ namespace project2
                 int bitsNumber = int.Parse(bits);
                 return bitsNumber >= 32 && bitsNumber % 8 == 0;
             }
-            catch (Exception ex)
+            catch
             {
                 Console.WriteLine(error_message);
             }
@@ -63,9 +75,29 @@ namespace project2
             return option == "prime" || option == "odd";
         }
 
-        static bool validCount(string option)
+        static Boolean validCount(string option)
         {
             return int.TryParse(option, out int result);
+        }
+
+        static byte[] getNumber(string bits)
+        {
+            int numberBytes = int.Parse(bits) / 8;
+            byte[] randomBytes = new byte[numberBytes];
+            RandomNumberGenerator generator = RandomNumberGenerator.Create();
+
+            generator.GetBytes(randomBytes);
+
+            return randomBytes;
+        }
+    }
+
+    public static class BigIntegerExtensions
+    {
+        public static bool IsProbablyPrime(this BigInteger value, int k = 10)
+        {
+            // put prime logic here
+            return false;
         }
     }
 }
